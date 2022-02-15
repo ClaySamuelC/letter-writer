6b9b6d30fc03d54c32d6cb8830ae1f5f24540b5e
Sam Clay <claysamuelc@gmail.com>
2022-02-15 13:32:12 -0500

    LetterWriter component now takes proper data for writing letters

--- ui/src/components/LetterWriter.js
+++ ui/src/components/LetterWriter.js
@@ -3,24 +3,35 @@ import { useInput } from './hooks/input-hook';
 import './LetterWriter.css';
 
 export function LetterWriter(props) {
-  const { value:firstName, bind:bindFirstName, reset:resetFirstName } = useInput('');
-  const { value:lastName, bind:bindLastName, reset:resetLastName } = useInput('');
+  const { value:author, bind:bindAuthor, reset:resetAuthor } = useInput('');
+  const { value:recipient, bind:bindRecipient, reset:resetRecipient } = useInput('');
+  const { value:heading, bind:bindHeading, reset:resetHeading } = useInput('');
+  const { value:message, bind:bindMessage, reset:resetMessage } = useInput('');
   
   const handleSubmit = (evt) => {
       evt.preventDefault();
-      alert(`Submitting Name ${firstName} ${lastName}`);
-      resetFirstName();
-      resetLastName();
+      alert(`Submitting Message`);
+      resetRecipient();
+      resetHeading();
+      resetMessage();
   }
   return (
     <form onSubmit={handleSubmit} className="LetterWriter">
       <label>
-        First Name:
-        <input type="text" {...bindFirstName} />
+        Your Name:
+        <input type="text" {...bindAuthor} />
       </label>
       <label>
-        Last Name:
-        <input type="text" {...bindLastName} />
+        Recipient
+        <input type="text" {...bindRecipient} />
+      </label>
+      <label>
+        Heading
+        <input type="text" {...bindHeading} />
+      </label>
+      <label>
+        Message
+        <input type="text" {...bindMessage} />
       </label>
       <input type="submit" value="Submit" />
     </form>
