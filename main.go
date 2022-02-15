e1cf73ecc1fe3f326564cb7be1dfa576191cccf5
Sam Clay <claysamuelc@gmail.com>
2022-02-15 13:50:58 -0500

    Add pings for getting letters and posting a letter with temporary message responses

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	router.GET("/api/letters", func(c *gin.Context) {
+		log.Printf("Getting letters")
+		c.JSON(200, gin.H{
+			"message": "Here are all the Letters on file!",
+		})
+	})
+
+	router.POST("/api/Letters", func(c *gin.Context) {
+		log.Printf("Writing Letter")
+		c.JSON(200, gin.H{
+			"message": "Letter written",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+	router.Run(":" + port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
