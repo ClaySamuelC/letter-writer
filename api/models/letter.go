78a205a1ad689fd4c0bf8fddf23bfd2b4309745a
Sam Clay <claysamuelc@gmail.com>
2022-02-15 15:23:24 -0500

    Add ToSlice function to Letter model for easy converting to csv format

--- api/models/letter.go
+++ api/models/letter.go
@@ -1,11 +1,14 @@
 package models
 
 type Letter struct {
-	ID       int64
-	fromName string
-	toName   string
-	heading  string
-	date     string
-	address  string
-	body     string
+	FromName string
+	ToName   string
+	Heading  string
+	Date     string
+	Address  string
+	Body     string
+}
+
+func (l Letter) ToSlice() []string {
+	return []string{l.FromName, l.ToName, l.Heading, l.Date, l.Address, l.Body}
 }
