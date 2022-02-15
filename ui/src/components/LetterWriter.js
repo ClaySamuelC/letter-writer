ff51971af7a6be56d5da008919c31d11d3ce7ab3
Sam Clay <claysamuelc@gmail.com>
2022-02-15 13:17:22 -0500

    Add react hooks (using https://rangle.io/blog/simplifying-controlled-inputs-with-hooks/ as a template)

---
+++ ui/src/components/LetterWriter.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useInput } from './hooks/input-hook';
+// import './LetterWriter.css';
+
+export function LetterWriter(props) {
+  const { value:firstName, bind:bindFirstName, reset:resetFirstName } = useInput('');
+  const { value:lastName, bind:bindLastName, reset:resetLastName } = useInput('');
+  
+  const handleSubmit = (evt) => {
+      evt.preventDefault();
+      alert(`Submitting Name ${firstName} ${lastName}`);
+      resetFirstName();
+      resetLastName();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input type="text" {...bindFirstName} />
+      </label>
+      <label>
+        Last Name:
+        <input type="text" {...bindLastName} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
