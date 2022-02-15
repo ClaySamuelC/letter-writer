bd02fa83e7e029b4efdd2ef33350074c8a86a3b9
Sam Clay <claysamuelc@gmail.com>
2022-02-15 15:27:26 -0500

    Add writeLetter route to letter controller

--- api/controllers/letter-controller.go
+++ api/controllers/letter-controller.go
@@ -1,9 +1,38 @@
 package controllers
 
 import (
+	"encoding/csv"
+
+	"github.com/ClaySamuelC/letter-writer/api/models"
 	"github.com/gin-gonic/gin"
 )
 
-func CreateLetter(ctx *gin.Context) {
+type LetterController struct {
+	db *csv.Writer
+}
 
+func (c *LetterController) Init(db *csv.Writer) {
+	c.db = db
+}
+
+func (c *LetterController) CreateLetter(ctx *gin.Context) {
+	var letter models.Letter
+	ctx.BindJSON(&letter)
+
+	if letter.Heading == "" {
+		ctx.JSON(400, gin.H{
+			"error": "heading should not be empty",
+		})
+		return
+	}
+	if err := c.db.Write(letter.ToSlice()); err != nil {
+		ctx.JSON(500, gin.H{
+			"error": "error writing letter to db",
+		})
+		return
+	}
+
+	ctx.JSON(201, gin.H{
+		"letter": letter,
+	})
 }
